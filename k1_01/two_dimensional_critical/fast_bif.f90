5042539dbc7f72544faca0c084ae823596877c65
Elle Musoke <elle.musoke@auckland.ac.nz>
2018-05-23 12:35:42 +1200

    23 May

---
+++ k1_01/two_dimensional_critical/fast_bif.f90
@@ -0,0 +1,94 @@
+!---------------------------------------------------------------------- 
+!---------------------------------------------------------------------- 
+!   Get bifurcation diagrams for a variety of k1 values
+!	You NEED to read the .auto files that correspond to the k1 values
+!	you want to run this with!!!!
+!---------------------------------------------------------------------- 
+!---------------------------------------------------------------------- 
+
+      SUBROUTINE FUNC(NDIM,U,ICP,PAR,IJAC,F,DFDU,DFDP) 
+!     ---------- ---- 
+
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: NDIM, ICP(*), IJAC
+      DOUBLE PRECISION, INTENT(IN) :: U(NDIM), PAR(*)
+      DOUBLE PRECISION, INTENT(OUT) :: F(NDIM)
+      DOUBLE PRECISION, INTENT(INOUT) :: DFDU(NDIM,NDIM), DFDP(NDIM,*)
+
+      DOUBLE PRECISION Period, a, b, x, y, alpha, mu, kappa,zeta,epsilon_sq,epsilon_b,delta,k1,k2,k3,k4,k5,k6,k7,kn7,k8
+	  
+	  !system variables
+	   x= U(1)
+	   y= U(2)
+	   	  
+	   !system parameters
+	   
+	   k1= PAR(1)
+	   k2= PAR(2)
+	   k3= PAR(3)
+	   k4= PAR(4)
+	   k5= PAR(5)
+	   k6= PAR(6)
+	   k7= PAR(7)
+	   kn7= PAR(8)
+	   k8= PAR(9)
+	   a = PAR(10)
+	   b = PAR(11)   
+	   
+	   mu= k7/k8
+	   alpha= (k1*k5*kn7)/(k3*k8*sqrt(2*k2*k8))
+	   epsilon_b= ((k1**2)*k5)/(2*k2*k3*k8)
+	   kappa= (sqrt(2*k2*k8))/k5
+	   epsilon_sq= (k3*k8)/(k1*k5)
+	   zeta= k4/(sqrt(2*k2*k8))
+	   delta= k6/k8
+	   
+       F(1)= (b*x-x**2 + 3*a*b*y - zeta*x + delta)/(epsilon_sq)
+	   F(2)= kappa*(x**2 - y - a*b*y)/(epsilon_sq)
+
+      END SUBROUTINE FUNC
+!---------------------------------------------------------------------- 
+      SUBROUTINE STPNT(NDIM,U,PAR,T) 
+!     ---------- ----- 
+
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: NDIM
+      DOUBLE PRECISION, INTENT(INOUT) :: U(NDIM),PAR(*)
+      DOUBLE PRECISION, INTENT(IN) :: T
+	  
+	  PAR(1:11)= (/ 0.1, 250.0, 0.035, 20.0, 5.35, 1E-4, 0.8, 0.1, 0.825, 2.0, 2.5 /)
+	  
+	  U(1)= -0.00008000018180448222123188235145393
+	  U(2)= 0.0000000010666715147917013778094844087038 !J
+	  
+      END SUBROUTINE STPNT
+!---------------------------------------------------------------------- 
+      SUBROUTINE BCND(NDIM,PAR,ICP,NBC,U0,U1,FB,IJAC,DBC) 
+!     ---------- ---- 
+
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: NDIM, ICP(*), NBC, IJAC
+      DOUBLE PRECISION, INTENT(IN) :: PAR(*), U0(NDIM), U1(NDIM)
+      DOUBLE PRECISION, INTENT(OUT) :: FB(NBC)
+      DOUBLE PRECISION, INTENT(INOUT) :: DBC(NBC,*)
+
+      END SUBROUTINE BCND
+!---------------------------------------------------------------------- 
+      SUBROUTINE ICND(NDIM,PAR,ICP,NINT,U,UOLD,UDOT,UPOLD,FI,IJAC,DINT) 
+!     ---------- ---- 
+
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: NDIM, ICP(*), NINT, IJAC
+      DOUBLE PRECISION, INTENT(IN) :: PAR(*)
+      DOUBLE PRECISION, INTENT(IN) :: U(NDIM), UOLD(NDIM), UDOT(NDIM), UPOLD(NDIM)
+      DOUBLE PRECISION, INTENT(OUT) :: FI(NINT)
+      DOUBLE PRECISION, INTENT(INOUT) :: DINT(NINT,*)
+
+
+      END SUBROUTINE ICND
+
+      SUBROUTINE PVLS
+      END SUBROUTINE PVLS
+
+      SUBROUTINE FOPT 
+      END SUBROUTINE FOPT
